bac4d5fba92c18661f4b2d75e9306324bcdf4c07
Henrique Queiroz <henriquereuter46@gmail.com>
2025-12-10 12:34:56 -0300

    Basic Routes

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
+require('dotenv').config()
+
+
 
 const express = require('express')
 const app = express()
 
 
+const connectDB = require('./db/connect')
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandler = require('./middleware/error-handler')
+
+app.use(express.json())
+
+app.get('/',(req,res) => {
+    res.send('e-commerce api')
+})
+
+app.use(notFoundMiddleware)
+app.use(errorHandler)
+
 const port = process.env.PORT || 5000
 const start  = async () => {
     try {
-        app.list(port,console.log(`Server is listening on port ${port}...`))
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port,console.log(`Server is listening on port ${port}...`))
     } catch (error) {
         console.log(error)        
     }
 }
+
+start()
