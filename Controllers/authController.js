4807a6a58d6ed849e78fd0971964f797f929d511
Henrique Queiroz <henriquereuter46@gmail.com>
2025-12-11 13:04:07 -0300

    Required email implemented and role set to user when it is not the firstAccount

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,17 +1,34 @@
-const register = async (req,res) => {
-    res.send('register')
-}
+const User = require("../models/User");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
 
-const login = async(req,res) => {
-    res.send('login user')
-}
+const register = async (req, res) => {
+  const { email, name, password } = req.body;
 
-const logout = async(req,res) => { 
-    res.send('logout user')
-}
+  const emailExists = await User.findOne({ email });
+  if (emailExists) {
+    throw new CustomError.BadRequestError(
+      "Esse email já foi usado, se inscreva com outro"
+    );
+  }
+
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? "admin" : "user";
+
+  const user = await User.create({ name, email, password, role });
+  res.status(StatusCodes.CREATED).json({ user });
+};
+
+const login = async (req, res) => {
+  res.send("login user");
+};
+
+const logout = async (req, res) => {
+  res.send("logout user");
+};
 
 module.exports = {
-    register,
-    login,
-    logout,
-}
+  register,
+  login,
+  logout,
+};
